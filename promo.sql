55c0bf933a9ea2d8092f791e222a4c6816754da1
CenkuSands <108849979+CenkuSands@users.noreply.github.com>
2024-08-30 15:24:20 +0800

    Update promo.sql

--- promo.sql
+++ promo.sql
@@ -1,30 +1,5 @@
-sum(
+max_over_time(sum(
   node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate{cluster="", namespace="prd-butler-mobile"}
 * on(namespace,pod)
   group_left(workload, workload_type) namespace_workload_pod:kube_pod_owner:relabel{cluster="", namespace="prd-butler-mobile", workload_type="deployment"}
-) by (workload, workload_type)
-/sum(
-  kube_pod_container_resource_limits{job="kube-state-metrics", cluster="", namespace="prd-butler-mobile", resource="cpu"}
-* on(namespace,pod)
-  group_left(workload, workload_type) namespace_workload_pod:kube_pod_owner:relabel{cluster="", namespace="prd-butler-mobile", workload_type="deployment"}
-) by (workload, workload_type)
-
-
-max_over_time(
-  (
-    sum(
-      node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate{cluster="", namespace="prd-butler-mobile"}
-      * on(namespace,pod)
-      group_left(workload, workload_type) 
-      namespace_workload_pod:kube_pod_owner:relabel{cluster="", namespace="prd-butler-mobile", workload_type="deployment"}
-    ) by (workload, workload_type)
-  )
-  /
-  sum(
-    kube_pod_container_resource_limits{job="kube-state-metrics", cluster="", namespace="prd-butler-mobile", resource="cpu"}
-    * on(namespace,pod)
-    group_left(workload, workload_type) 
-    namespace_workload_pod:kube_pod_owner:relabel{cluster="", namespace="prd-butler-mobile", workload_type="deployment"}
-  ) by (workload, workload_type)
-)[1w:]
-)
+) [1w:]) by (workload, workload_type)
